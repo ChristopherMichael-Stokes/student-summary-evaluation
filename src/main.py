ce08647d99c969fc9a7ba7c1b97a78b10cda7a4f
Christopher Michael-Stokes <christophermichaelstokes@gmail.com>
2023-07-31 21:49:12 +0100

    Add TODOs

--- src/main.py
+++ src/main.py
@@ -1,20 +1,21 @@
-import hydra
-import torch
-import transformers
-import nltk
 import accelerate
 import bitsandbytes
-import peft 
-import xformers
 import evaluate
-import scipy
+import hydra
+import nltk
 import numpy
 import pandas
+import peft
+import scipy
+import torch
+import transformers
+import xformers
 
 
 @hydra.main('../conf', 'conf.yaml', version_base='1.2')
 def main(cfg):
     pass
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
